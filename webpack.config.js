24e91aafeacc5be937759f44d98dd7a5faecab01
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-03-13 19:41:51 -0700

    Add webpack file and index template

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'react-dom',
+];
+
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    bundle: './src/index.jsx',
+    vendor: VENDOR_LIBS,
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: [
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                sourceMaps: true,
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [
+                    require('postcss-import'),
+                    require('postcss-css-variables'),
+                    require('autoprefixer'),
+                    require('postcss-custom-media'),
+                    require('postcss-import'),
+                    require('postcss-nested'),
+                    require('postcss-color-function'),
+                  ];
+                },
+              },
+            },
+          ],
+        }),
+      },
+      {
+        use: 'file-loader?[name].[hash].[ext]',
+        test: /\.jpg$|\.png$|\.svg$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.tmpl.html',
+      filename: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
