a2817d6316330fa8c605a13ebd06494a65beed05
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-04-11 23:00:59 -0700

    Use dist instead of build in server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 const port = (process.env.PORT || 8080);
 
-const indexPath = path.join(__dirname, './build/index.html');
-const publicPath = express.static(path.join(__dirname, './build'));
+const indexPath = path.join(__dirname, './dist/index.html');
+const publicPath = express.static(path.join(__dirname, './dist'));
 
-app.use('/build', publicPath);
-app.get('*/', function(_, res) { res.sendFile(indexPath) });
+app.use('/dist', publicPath);
+app.get('*', function(_, res) { res.sendFile(indexPath) });
 
 app.listen(port, console.log(`Production server running on port ${port}`))
 
