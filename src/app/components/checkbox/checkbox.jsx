40f6250b6e9e9181698a58fa8243147dcbfa0bea
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-05-11 21:46:44 -0700

    Add onKeyDown to checkbox

--- src/app/components/checkbox/checkbox.jsx
+++ src/app/components/checkbox/checkbox.jsx
@@ -1,22 +1,33 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './checkbox.css';
+import autobind from 'react-autobind';
 
 class Checkbox extends Component {
-  propTypes = {
+  static propTypes = {
     onChange: PropTypes.func,
     checked: PropTypes.bool,
+  }
+
+  constructor(props) {
+    super(props);
+    autobind(this);
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode === 13) {
+      this.props.onChange(e);
+    }
   }
 
   render() {
     const { checked, onChange } = this.props;
 
     return (
-      <div className={styles['checkbox']}>
+      <div className={styles['checkbox']} tabIndex={0} onKeyDown={this.onKeyDown}>
         <input 
           type="checkbox"
           onChange={onChange}
           checked={checked}
-          ref={(checkbox) => this._checkbox = checkbox}
         />
         <label onClick={onChange}>
           {this.props.children}
